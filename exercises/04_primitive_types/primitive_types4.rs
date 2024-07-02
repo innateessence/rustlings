c076f69fdbca81688f42422bbb2d73a79cbbe7dc
JackofSpades707 <jackofspades707@hotmail.com>
2024-07-01 20:42:40 -0700

    chapter 4

--- exercises/04_primitive_types/primitive_types4.rs
+++ exercises/04_primitive_types/primitive_types4.rs
@@ -5,13 +5,15 @@
 // Execute `rustlings hint primitive_types4` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
+fn slice(arr: &[i8; 5], start: usize, end: usize) -> &[i8] {
+    return &arr[start..end];
+}
 
 #[test]
 fn slice_out_of_array() {
     let a = [1, 2, 3, 4, 5];
 
-    let nice_slice = ???
+    let nice_slice = slice(&a, 1, 4);
 
     assert_eq!([2, 3, 4], nice_slice)
 }
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,13 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num: i8) -> i8 {
+    if num <= 40 {
+        return num * 2
+    }
+    return num
+}
 
 // Don't modify this function!
 #[test]
