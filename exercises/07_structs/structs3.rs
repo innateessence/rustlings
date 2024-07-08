26b68a5b325b26a8a5c6e77e77b259367d749398
JackofSpades707 <jackofspades707@hotmail.com>
2024-07-08 14:55:53 -0700

    chapter 7

--- exercises/07_structs/structs3.rs
+++ exercises/07_structs/structs3.rs
@@ -7,8 +7,6 @@
 // Execute `rustlings hint structs3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 struct Package {
     sender_country: String,
@@ -31,12 +29,18 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
-        // Something goes here...
+    fn is_international(&self) -> bool {
+        // Looks like Canada is considered local when reading test cases
+        let s = self.sender_country.as_str();
+        let retval = match s {
+            "Canada" => false,
+            _ => true,
+        };
+        retval
     }
 
-    fn get_fees(&self, cents_per_gram: u32) -> ??? {
-        // Something goes here...
+    fn get_fees(&self, cents_per_gram: u32) -> u32 {
+        self.weight_in_grams * cents_per_gram
     }
 }
 
